9df153802279e63cae1c6e272c72f60f74172cf2
jontonsoup4 <jontonsoup4@gmail.com>
2015-11-11 18:56:04 -0600

    added image resizing

--- to_picture.py
+++ to_picture.py
@@ -13,16 +13,25 @@ class ASCIIPicture:
         width, height = d.textsize(text)
         self.width = width
         self.height = height
-        self.img = Image.new('RGB', (width, height), (255, 255, 255))
+        self.ratio = self.width / self.height
+        self.img = Image.new('RGB', (self.width, self.height), (255, 255, 255))
         d = ImageDraw.Draw(self.img)
         d.text((0, 0), text, fill=(0, 0, 0))
 
-    def save(self, save_name):
+    def save(self, save_name, new_height=0):
         """
         :param save_name: path to desired save location
+        :param new_height: optional parameter for new y size
 
         """
-        self.img.save(save_name)
+        if new_height == 0:
+            self.img.save(save_name)
+        else:
+            self.height = new_height
+            self.width = int(new_height * self.ratio)
+            self.img = self.img.resize((self.width, self.height), Image.BILINEAR)
+            self.img.save(save_name)
+
 
     def show(self):
         self.img.show()
